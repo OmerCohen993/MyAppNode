01ffab3d1cef8b6e4460a749bd7be3076e8b427f
OmerCohen993 <omerplace@gmail.com>
2023-06-30 01:08:48 +0300

    general exeption 404

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.use((req, res, next) => {
     }
     next();
 });
+
+//exeption in case not root found
+app.use((req, res, next) => {
+    const error = new Error('Not Found beaiitchhhh');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
